4f6cf7d36ae14f608df075e2cfaad1e0d209852a
Jorge Silva <jorgemsrs@gmail.com>
2020-04-28 09:59:22 +0100

    Improved coverage

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,10 +1,15 @@
 "use strict";
 
+const sinon = require('sinon');
+const proxyquire = require('proxyquire').noCallThru();
+
 const VICTIM_PATH = "../../";
 
-const setup = () => {
+const setup = (stubs) => {
   const BaseJoi = require("@hapi/joi");
-  const FileExtension = require(VICTIM_PATH);
+  let FileExtension;
+  if (stubs) FileExtension = proxyquire(VICTIM_PATH, stubs);
+  else FileExtension = require(VICTIM_PATH);
 
   return BaseJoi.extend(FileExtension);
 };
@@ -45,7 +50,7 @@ test("Encoding must be 'utf8' or null", function(t) {
   }
 });
 
-test("Fails when file can't be read", function(t) {
+test("Fails when file does not exist", function(t) {
   t.plan(2);
 
   const Joi = setup();
@@ -57,6 +62,20 @@ test("Fails when file can't be read", function(t) {
 
   t.ok(error !== null);
   t.equal(error.message, expectedError);
+});
+
+test("When no explicit encoding is specified, file contents are returned as Buffer", function(t) {
+  t.plan(2);
+
+  const filename = `/tmp/jfe-test-${Date.now()}`;
+  const expectedContent = `just a test ${Date.now()}`;
+  const Joi = setupWithFile(filename, expectedContent);
+  const schema = Joi.file().contents();
+
+  const { value, error } = schema.validate(filename);
+
+  t.ok(error === undefined);
+  t.deepEqual(value, Buffer.from(expectedContent));
 });
 
 test("Get file contents as Buffer", function(t) {
@@ -85,4 +104,45 @@ test("Get file contents as String (utf8)", function(t) {
 
   t.ok(error === undefined);
   t.deepEqual(value, expectedContent);
+});
+
+test("Fails getting contents when readFileSync throws an unsupported error code", function(t) {
+  t.plan(2);
+
+  const e = new Error("cenas");
+  const filename = Date.now().toString();
+  const stubs = {
+    fs: {
+      existsSync: sinon.stub().onFirstCall().returns(true),
+      readFileSync: sinon.stub().onFirstCall().throws(e)
+    }
+  }
+  const Joi = setup(stubs);
+
+  const schema = Joi.file().contents({ encoding: "utf8" });
+  const { value, error } = schema.validate(filename);
+
+  t.equal(value, filename);
+  t.equal(error.message, `file could not be read. message: "${e.message}"`)
+});
+
+test("Fails getting contents when readFileSync throws ENOENT error code", function(t) {
+  t.plan(2);
+
+  const e = new Error("cenas");
+  e.code = 'ENOENT';
+  const filename = Date.now().toString();
+  const stubs = {
+    fs: {
+      existsSync: sinon.stub().onFirstCall().returns(true),
+      readFileSync: sinon.stub().onFirstCall().throws(e)
+    }
+  }
+  const Joi = setup(stubs);
+
+  const schema = Joi.file().contents({ encoding: "utf8" });
+  const { value, error } = schema.validate(filename);
+
+  t.equal(value, filename);
+  t.equal(error.message, `file "${filename}" does not exist`)
 });
